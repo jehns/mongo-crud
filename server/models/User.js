36ad2209a71132b46e399b7e560d069a97c968fa
Stuart Jehn <stuartjehn@gmail.com>
2019-04-02 18:56:51 -0400

    deployment script / action complete

--- server/models/User.js
+++ server/models/User.js
@@ -8,8 +8,14 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true
+  },
+  password: {
+    type: String,
+    required: true
   }
 })
+
+
 
 const User = mongoose.model('User', UserSchema);
 
